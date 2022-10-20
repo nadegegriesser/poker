3f6170f3db676d992144ac6daaa979579f47ecd0
Griesser, Nadege <Nadege.Griesser@vector.com>
2022-10-20 19:03:06 +0200

    implement and test predicates for hand type

--- src/main/java/de/griesser/poker/HandType.java
+++ src/main/java/de/griesser/poker/HandType.java
@@ -1,18 +1,26 @@
 package de.griesser.poker;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public enum HandType {
-    STRAIGHT_FLUSH((Hand hand) -> true, (Hand hand1, Hand hand2) -> 0),
-    FOUR_OF_A_KIND((Hand hand) -> true, (Hand hand1, Hand hand2) -> 0),
-    FULL_HOUSE((Hand hand) -> true, (Hand hand1, Hand hand2) -> 0),
-    FLUSH((Hand hand) -> true, (Hand hand1, Hand hand2) -> 0),
-    STRAIGHT((Hand hand) -> true, (Hand hand1, Hand hand2) -> 0),
-    THREE_OF_A_KIND((Hand hand) -> true, (Hand hand1, Hand hand2) -> 0),
-    TWO_PAIRS((Hand hand) -> true, (Hand hand1, Hand hand2) -> 0),
-    PAIR((Hand hand) -> true, (Hand hand1, Hand hand2) -> 0),
+    STRAIGHT_FLUSH((Hand hand) -> hasStraight(hand) && hasFlush(hand), (Hand hand1, Hand hand2) -> 0),
+    FOUR_OF_A_KIND((Hand hand) -> has4OfAKind(hand), (Hand hand1, Hand hand2) -> 0),
+    FULL_HOUSE((Hand hand) -> hasFullHouse(hand), (Hand hand1, Hand hand2) -> 0),
+    FLUSH((Hand hand) -> hasFlush(hand), (Hand hand1, Hand hand2) -> 0),
+    STRAIGHT((Hand hand) -> hasStraight(hand), (Hand hand1, Hand hand2) -> 0),
+    THREE_OF_A_KIND((Hand hand) -> has3OfAKind(hand), (Hand hand1, Hand hand2) -> 0),
+    TWO_PAIRS((Hand hand) -> hasTwoPairs(hand), (Hand hand1, Hand hand2) -> 0),
+    PAIR((Hand hand) -> hasPair(hand), (Hand hand1, Hand hand2) -> 0),
     HIGH_CARD((Hand hand) -> true, (Hand hand1, Hand hand2) -> 0);
 
     private final Predicate<Hand> predicate;
@@ -29,5 +37,62 @@ public enum HandType {
 
     public static HandType getHandType(Hand hand) {
         return Arrays.stream(values()).filter(type -> type.predicate.test(hand)).findFirst().get();
+    }
+
+    private static boolean hasPair(Hand hand) {
+        return hasCountsOfAKind(hand, Arrays.asList(2));
+    }
+
+    private static boolean has3OfAKind(Hand hand) {
+        return hasCountsOfAKind(hand, Arrays.asList(3));
+    }
+
+    private static boolean has4OfAKind(Hand hand) {
+        return hasCountsOfAKind(hand, Arrays.asList(4));
+    }
+
+    private static boolean hasFullHouse(Hand hand) {
+        return hasCountsOfAKind(hand, Arrays.asList(3, 2));
+    }
+
+    private static boolean hasTwoPairs(Hand hand) {
+        return hasCountsOfAKind(hand, Arrays.asList(2, 2));
+    }
+
+    private static boolean hasCountsOfAKind(Hand hand, List<Integer> counts) {
+        Map<CardValue, Integer> countByValue = getCountByValue(hand);
+        for (Integer count : counts) {
+            if (!countByValue.values().remove(count)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static Map<CardValue, Integer> getCountByValue(Hand hand) {
+        Map<CardValue, Integer> countByValue = new HashMap<>();
+        for (Card card : hand.getCards()) {
+            countByValue.merge(card.getValue(), 1, Integer::sum);
+        }
+        return countByValue;
+    }
+
+    private static boolean hasFlush(Hand hand) {
+        Map<CardSuit, Set<CardValue>> valuesBySuit = new HashMap<>();
+        for (Card card : hand.getCards()) {
+            valuesBySuit.computeIfAbsent(card.getSuit(), k -> new HashSet<>()).add(card.getValue());
+        }
+        return valuesBySuit.size() == 1;
+    }
+
+    private static boolean hasStraight(Hand hand) {
+        Set<CardValue> values = hand.getCards().stream().map(card -> card.getValue()).collect(Collectors.toSet());
+        if (values.size() != hand.getCards().size()) {
+            return false;
+        }
+        List<CardValue> sortedValues = new ArrayList<>(values);
+        Collections.sort(sortedValues);
+        return (sortedValues.get(sortedValues.size() - 1).ordinal() - sortedValues.get(0).ordinal()) == (values.size()
+                - 1);
     }
 }
---
+++ src/test/java/de/griesser/poker/HandTypeTest.java
@@ -0,0 +1,90 @@
+package de.griesser.poker;
+
+import static de.griesser.poker.CardSuit.*;
+import static de.griesser.poker.CardValue.*;
+import static de.griesser.poker.HandType.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Set;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class HandTypeTest {
+
+        @ParameterizedTest(name = "{index}: {0} -> {1}")
+        @MethodSource("provideHandTypes")
+        public void getHandType_ShouldReturnAppropriateValue(Hand hand, HandType expected) {
+                assertEquals(expected, HandType.getHandType(hand));
+        }
+
+        private static Stream<Arguments> provideHandTypes() {
+                return Stream.of(
+                                Arguments.of(
+                                                new Hand(Set.of(new Card(SPADE, TWO),
+                                                                new Card(SPADE, THREE),
+                                                                new Card(SPADE, FIVE),
+                                                                new Card(DIAMOND, SIX),
+                                                                new Card(DIAMOND, NINE))),
+                                                HIGH_CARD),
+                                Arguments.of(
+                                                new Hand(Set.of(new Card(CLUB, JACK),
+                                                                new Card(DIAMOND, JACK),
+                                                                new Card(CLUB, TWO),
+                                                                new Card(CLUB, THREE),
+                                                                new Card(CLUB, FOUR))),
+                                                PAIR),
+                                Arguments.of(
+                                                new Hand(Set.of(new Card(CLUB, SEVEN),
+                                                                new Card(DIAMOND, SEVEN),
+                                                                new Card(CLUB, NINE),
+                                                                new Card(DIAMOND, NINE),
+                                                                new Card(HEART, TEN))),
+                                                TWO_PAIRS),
+                                Arguments.of(
+                                                new Hand(Set.of(new Card(DIAMOND, TWO),
+                                                                new Card(HEART, TWO),
+                                                                new Card(SPADE, TWO),
+                                                                new Card(CLUB, FIVE),
+                                                                new Card(CLUB, SIX))),
+                                                THREE_OF_A_KIND),
+                                Arguments.of(
+                                                new Hand(Set.of(new Card(DIAMOND, THREE),
+                                                                new Card(DIAMOND, FOUR),
+                                                                new Card(HEART, FIVE),
+                                                                new Card(HEART, SIX),
+                                                                new Card(HEART, SEVEN))),
+                                                STRAIGHT),
+                                Arguments.of(
+                                                new Hand(Set.of(new Card(CLUB, TWO),
+                                                                new Card(CLUB, THREE),
+                                                                new Card(CLUB, FOUR),
+                                                                new Card(CLUB, SEVEN),
+                                                                new Card(CLUB, NINE))),
+                                                FLUSH),
+                                Arguments.of(
+                                                new Hand(Set.of(new Card(CLUB, SIX),
+                                                                new Card(DIAMOND, SIX),
+                                                                new Card(HEART, SIX),
+                                                                new Card(CLUB, TWO),
+                                                                new Card(DIAMOND, TWO))),
+                                                FULL_HOUSE),
+                                Arguments.of(
+                                                new Hand(Set.of(new Card(CLUB, SEVEN),
+                                                                new Card(DIAMOND, SEVEN),
+                                                                new Card(HEART, SEVEN),
+                                                                new Card(SPADE, SEVEN),
+                                                                new Card(CLUB, EIGHT))),
+                                                FOUR_OF_A_KIND),
+                                Arguments.of(
+                                                new Hand(Set.of(new Card(CLUB, THREE),
+                                                                new Card(CLUB, FOUR),
+                                                                new Card(CLUB, FIVE),
+                                                                new Card(CLUB, SIX),
+                                                                new Card(CLUB, SEVEN))),
+                                                STRAIGHT_FLUSH));
+        }
+
+}
